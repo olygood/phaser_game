afe09298b0bc58fdda8b34881464a11b4f5f389b
olygood <olivier.huttmacher@gmail.com>
2019-11-22 15:30:13 +0100

    server res- <h1></h1>

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 var http = require("http"); //server http
-var url =  require("url"); // lire les url
-var fs = require("fs"); //file system lire les fichier system
+var url  =  require("url"); // lire les url
+var fs   = require("fs"); //file system lire les fichier system
 
 const PORT = "8080";
 var server  = http.createServer(traitReq);
 server.listen(PORT);
 
 function traitReq(req, res){
-    console.log("coucou");
+    res.writeHead(200,{"Content-type":"text/html"});
+    res.write("<h1>phaser game</h1>");
+    res.end();
+    
+
 }
